93e91b6a1e033e85d5e83560c856fc5545a0c179
Olexandr Morar <olexandr.morar@gmail.com>
2023-07-22 21:37:26 +0300

    init

---
+++ src/scripts/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDE2eZOzVGpbT0s2LB6pZHlEBUSNwWOv-Q',
+  authDomain: 'ts-todo-project.firebaseapp.com',
+  projectId: 'ts-todo-project',
+  storageBucket: 'ts-todo-project.appspot.com',
+  messagingSenderId: '198650447364',
+  appId: '1:198650447364:web:b98edd512f6348bbf65751',
+  measurementId: 'G-5JDDM3CVY5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
